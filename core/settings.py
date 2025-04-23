ed5b4b7bd03bfdf68d84af3d21eb54cdee6225c8
Ruan C4rlos <ruancarlos@ufpi.edu.br>
2025-04-23 10:40:01 -0300

    criando filtros com RQL- django-rql

--- core/settings.py
+++ core/settings.py
@@ -162,5 +162,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': ['dj_rql.drfRQLFilterBackend'],
     
 }
--- vehicles/views.py
+++ vehicles/views.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import DjangoModelPermissions, IsAdminUser
 from rest_framework import viewsets
 
 from core.permissions import IsOwnerOfVehicleOrRecord
-
+from vehicles.filters import VehicleTypeFilterClass, VehicleFilterClass
 from vehicles.models import Vehicle, VehicleType
 from vehicles.serializers import VehicleSerializer, VehicleTypeSerializer
 
@@ -10,12 +10,14 @@ from vehicles.serializers import VehicleSerializer, VehicleTypeSerializer
 class VehicleTypeViewSet(viewsets.ModelViewSet):
     queryset = VehicleType.objects.all()
     serializer_class = VehicleTypeSerializer
+    rql_filter_class = VehicleTypeFilterClass
     permission_classes = [DjangoModelPermissions, IsAdminUser]
 
 
 class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+    rql_filter_class = VehicleFilterClass
     permission_classes = [DjangoModelPermissions, IsOwnerOfVehicleOrRecord]
 
     def get_queryset(self):
